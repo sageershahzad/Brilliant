723209880906cb108002f7537afb83e55590c4de
Sageer Shahzad <rudph1@gmail.com>
2019-04-15 02:27:29 +0100

    Filteration Added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
         id: "sageer",
         name: "Sageer S",
         email: "sageer@facebook.com",
-        avatarURL: "https://avatars1.githubusercontent.com/u/5566246?s=460&v=4"
+        avatarURL:
+          "https://media.licdn.com/dms/image/C4D03AQFyYD0hzvchEw/profile-displayphoto-shrink_100_100/0?e=1560988800&v=beta&t=upUQC6GGKpqhNGSHaBwV-QU3wGhAs_HTEWE6_lJP6ok"
       },
       {
         id: "syed",
@@ -23,6 +24,27 @@ class App extends Component {
         email: "umer@facebook.com",
         avatarURL:
           "https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-1/p240x240/29683496_1332652363503148_5333555374651800969_n.jpg?_nc_cat=106&_nc_ht=scontent-lht6-1.xx&oh=805ee360befc2b807ce840635557c768&oe=5D2E4BAA"
+      },
+      {
+        id: "fahad",
+        name: "Fahad A",
+        email: "fahad@facebook.com",
+        avatarURL:
+          "https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/10565236_475445459225673_7948776937743984778_n.jpg?_nc_cat=111&_nc_ht=scontent-lht6-1.xx&oh=a6d2e1c6c503284c2e22654faf7bc063&oe=5D455718"
+      },
+      {
+        id: "mahira",
+        name: "Mahira K",
+        email: "mahira@facebook.com",
+        avatarURL:
+          "https://scontent-lht6-1.xx.fbcdn.net/v/t31.0-8/27164176_2021998321410559_6542261705151719075_o.jpg?_nc_cat=102&_nc_ht=scontent-lht6-1.xx&oh=75b4bf919540ce0fd1cddd03a02acb57&oe=5D78207E"
+      },
+      {
+        id: "mehwish",
+        name: "Mehwish H",
+        email: "mehwish@facebook.com",
+        avatarURL:
+          "https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/22045733_10156610510098222_392426775281448896_n.jpg?_nc_cat=104&_nc_ht=scontent-lht6-1.xx&oh=05e921681670e36c8648b08b8e507761&oe=5D4191E7"
       }
     ]
   };
--- src/components/listContacts.jsx
+++ src/components/listContacts.jsx
@@ -1,39 +1,97 @@
-//import React, { Component } from "react";
-import React from "react";
+import React, { Component } from "react";
+//import React from "react";
 import PropTypes from "prop-types";
-function ListContacts(props) {
-  // const { contacts } = this.props;
-  return (
-    <ol className="contact-list">
-      {props.contacts.map(contact => (
-        <li key={contact.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}
+import escapeRegExp from "escape-string-regexp";
+import sortBy from "sort-by";
+class ListContacts extends Component {
+  //const { contacts } = this.props;
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  };
+  state = {
+    query: ""
+  };
+  updateQuery = query => {
+    this.setState({ query: query.trim() });
+  };
+
+  // This will show all
+  clearQuery = () => {
+    this.setState({ query: "" });
+  };
+
+  render() {
+    const { contacts, onDeleteContact } = this.props;
+    const { query } = this.state;
+    let showingContacts;
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), "i");
+      showingContacts = contacts.filter(contact => match.test(contact.name));
+    } else {
+      showingContacts = contacts;
+    }
+
+    // Sorting Name
+    showingContacts.sort(sortBy("name"));
+
+    return (
+      <div className="list-contacts">
+        {/* {JSON.stringify(this.state)} */}
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Seach Contacts"
+            value={this.state.query}
+            onChange={event => this.updateQuery(event.target.value)}
           />
+        </div>
 
-          <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
+        {/* Show length */}
+        {showingContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>
+              Now Showing {showingContacts.length} of {contacts.length}
+            </span>
+            <button onClick={this.clearQuery}>Show All</button>
           </div>
-          <div>
-            <button
-              onClick={() => props.onDeleteContact(contact)}
-              className="contact-remove"
-            >
-              Remove
-            </button>
-          </div>
-        </li>
-      ))}
-    </ol>
-  );
+        )}
+
+        <ol className="contact-list">
+          {/* instead this.props.contacts useshowingContacts */}
+
+          {showingContacts.map(contact => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }}
+              />
+
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <div>
+                <button
+                  onClick={() => onDeleteContact(contact)}
+                  className="contact-remove"
+                >
+                  Remove
+                </button>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
+// ListContacts.propTypes = {
+//   contacts: PropTypes.array.isRequired,
+//   onDeleteContact: PropTypes.func.isRequired
+// };
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
-};
 export default ListContacts;
